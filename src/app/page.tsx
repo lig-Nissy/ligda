16d36519c9a3e2939b62c0010f026b81909a146b
nishizawa Yuya <nishizawa_yuya@liginc.co.jp>
2026-01-19 18:21:56 +0900

    feat:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { GameMenu, GameScreen, Ranking } from "@/features/game";
 import { Difficulty } from "@/types";
-import { BgmController } from "@/components/BgmController";
 
 type Screen = "menu" | "game";
 
@@ -27,7 +26,6 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-zinc-950 p-4">
-      <BgmController />
       {screen === "menu" && (
         <div className="relative w-full max-w-xl">
           <GameMenu onStart={handleStart} onDifficultyChange={setMenuDifficulty} />
